984d6b6346df6ea5b36d9b8bc6eaceb0eefa7cea
muhammadferdiherdiansyah25 <101733752+muhammadferdiherdiansyah25@users.noreply.github.com>
2022-06-01 19:57:20 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Contoh Modularisasi</title>
+<link href="style.css" rel="stylesheet" type="text/stylesheet"
+media="screen" />
+</head>
+<body>
+<div class="container">
+<header>
+<h1>Modularisasi Menggunakan Require</h1>
+</header>
+<nav>
+<a href="home.php">Home</a>
+<a href="about.php">Tentang</a>
+<a href="kontak.php">Kontak</a>
+</nav>
+
+<?php
+error_reporting(E_ALL);
+include_once 'koneksi.php';
+
+if (isset($_POST['submit']))
+{
+    $nama = $_POST['nama'];
+    $kategori = $_POST['kategori'];
+    $harga_jual = $_POST['harga_jual'];
+    $harga_beli = $_POST['harga_beli'];
+    $stok = $_POST['stok'];
+    $file_gambar = $_FILES['file_gambar'];
+    $gambar = null;
+
+    if ($file_gambar['error'] == 0)
+    {
+        $filename = str_replace(' ', '_',$file_gambar['name']);
+        $destination = dirname(_FILE_) .'/gambar/' . $filename;
+        if(move_uploaded_file($file_gambar['tmp_name'], $destination))
+        {
+            $gambar = 'gambar/' . $filename;;
+        }
+    }
+    
+    $sql = 'INSERT INTO data_barang (nama, kategori, harga_jual, harga_beli, stok, gambar) ';
+    $sql .= "VALUE ('{$nama}', '{$kategori}','{$harga_jual}','{$harga_beli}', '{$stok}', '{$gambar}')";
+    $result = mysqli_query($conn, $sql);
+    header('location: index.php');
+}
+?>
+
